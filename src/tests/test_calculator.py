4f492df00985054f17c1a303fd359e9870c7e13d
snely <dev-snely@gmail.com>
2025-09-12 05:40:03 -0400

    [test] ajout des tests : substraction, multiplication, division, division_by_zero

--- src/tests/test_calculator.py
+++ src/tests/test_calculator.py
@@ -13,3 +13,19 @@ def test_app():
 def test_addition():
     my_calculator = Calculator()
     assert my_calculator.addition(2, 3) == 5
+
+def test_subtraction():
+    my_calculator = Calculator()
+    assert my_calculator.subtraction(5, 3) == 2
+
+def test_multiplication():
+    my_calculator = Calculator()
+    assert my_calculator.multiplication(4, 3) == 12
+
+def test_division():
+    my_calculator = Calculator()
+    assert my_calculator.division(10, 2) == 5
+
+def test_division_by_zero():
+    my_calculator = Calculator()
+    assert my_calculator.division(10, 0) == "Erreur : division par zéro"
