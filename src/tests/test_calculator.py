62e9a300cad435be119f16ab5df08ec3008e474d
snely <dev-snely@gmail.com>
2025-09-12 05:35:00 -0400

    [test] new test : test_addition

--- src/tests/test_calculator.py
+++ src/tests/test_calculator.py
@@ -4,10 +4,12 @@ SPDX - License - Identifier: LGPL - 3.0 - or -later
 Auteurs : Gabriel C. Ullmann, Fabio Petrillo, 2025
 """
 
-from calculator import Calculator
+from main.calculator import Calculator
 
 def test_app():
     my_calculator = Calculator()
     assert my_calculator.get_hello_message() == "== Calculatrice v1.0 =="
 
-# TODO: ajoutez les tests
+def test_addition():
+    my_calculator = Calculator()
+    assert my_calculator.addition(2, 3) == 5
